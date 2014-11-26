488e10b483767df4de94f8435915110818569a0e
SAM LOW <SAMUEL.P.LOW@GMAIL.COM>
2014-11-26 16:03:26 +0000

    Bring LL branch up to date. Discarded any changes made to James' work. Created Level Builder subdir. Authored changes to a version of newlab3 in that subdir

--- src/Lin_Low/puzzle.c
+++ src/Lin_Low/puzzle.c
@@ -2,18 +2,9 @@
 
 #include <stdlib.h>
 #include "display.h"
-#include "crac.h"
 
 void run(){
 
-    Display d = newDisplay();
-
-    while(getEvent(d) == false){
-	    startFrame(d);
-	    drawBall(d,0.5,0.5);
-    	endFrame(d);
-	}
-    quit(d);
 }
 
 int main(){ 
