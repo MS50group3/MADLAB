d8920d08282ac6e3535a5f5f1baabe36db3b37eb
JamesCollerton <jamescollerton@live.co.uk>
2014-12-17 16:21:44 +0000

    No local copy

--- src/Old_Integrations/Current_Integration_2/crac.h
+++
@@ -1,4 +0,0 @@
-#include <SDL2/SDL.h>
-
-void draw(SDL_Renderer *renderer, SDL_Rect rcSprite, SDL_Rect rcSrc, SDL_Window *window);
-void run();
--- src/Old_Integrations/Working_Integration_Seven/movement.c
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "SDL2/SDL.h"
-#include <stdbool.h>
